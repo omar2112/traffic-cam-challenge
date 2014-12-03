3a488b5c7615e48549b41e6fdf946185ace8376c
Omar Rojas <omar2112@uw.edu>
2014-12-03 01:32:24 -0800

    end of 12/3

--- js/app.js
+++ js/app.js
@@ -1,13 +1,76 @@
-// List of Seattle Traffic Cameras
-// http://data.seattle.gov/resource/65fc-btcc.json
-
 "use strict";
 
-//put your code here to create the map, fetch the list of traffic cameras
-//and add them as markers on the map
-//when a user clicks on a marker, you should pan the map so that the marker
-//is at the center, and open an InfoWindow that displays the latest camera
-//image
-//you should also write the code to filter the set of markers when the user
-//types a search phrase into the search box
+$(document).ready(function() {
+	var mapElem = document.getElementById('map');
+	var searchbox = $('#search');
 
+	function createMap(elem, center, zoom) {
+		var map = new google.maps.Map(elem, {
+			center: center, 
+			zoom: zoom,
+		});
+	}
+
+	var center = {
+		lat: 47.6,
+		lng: -122.3
+	};
+
+	var map = new google.maps.Map(mapElem, {
+		center: center,
+		zoom: 12
+	});
+
+	var infoWindow = new google.maps.InfoWindow();
+	infoWindow.setContent("<h2>Here I am!</h2><p>Don't you wish you were here.</p>");
+
+	var focuspoints;
+	var markers = [];
+
+	$.getJSON('http://data.seattle.gov/resource/65fc-btcc.json') 
+		.done(function(data) {
+			focuspoints = data;
+
+			//create a new marker with the corresponding position
+			data.forEach(function(focuspoint) {
+				var marker = new google.maps.Marker({
+					position: {
+						lat: Number(focuspoint.location.latitude),
+						lng: Number(focuspoint.location.longitude)
+					},
+					map: map
+				});
+				markers.push(marker);
+
+				//assings the infowindow according to the current marker's picture and name.
+				google.maps.event.addListener(marker, 'click', function() {
+					map.panTo(this.getPosition());
+					var html = '<h2>' + focuspoint.cameralabel + '</h2>';
+					html+= '<p><img src=' + focuspoint.imageurl.url + '></p>';
+					infoWindow.setContent(html);
+					infoWindow.open(map, this);
+				});
+			})
+
+		})
+		.fail(function(error) {
+			alert('traffic data failed to load! ' + error);
+		})
+		.always(function() {
+
+		});
+
+		//only markers that contain the search phrase will appear. 
+		searchbox.bind('search keyup', function() {
+			for (var i = 0; i < focuspoints.length; i++) {
+				var tempString = focuspoints[i].cameralabel.toLowerCase();
+				var compareString = this.value.toLowerCase();
+				if (tempString.indexOf(compareString) == -1) {
+					markers[i].setMap(null);
+				} else {
+					markers[i].setMap(map);
+				}
+			}
+		});
+
+});
